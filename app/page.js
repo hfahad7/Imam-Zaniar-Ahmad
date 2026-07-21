d0407cc12c310009c21d9beba279f63f8f391516
Hamiz Fahad <hamizf09@gmail.com>
2026-07-21 13:06:47 -0400

    text

--- app/page.js
+++ app/page.js
@@ -13,6 +13,8 @@ export const metadata = buildMetadata({
 });
 
 export default function Home() {
+  throw new Error('Test error');
+
   return (
     <div className="page-shell home-page">
       <section className="hero hero--home">
